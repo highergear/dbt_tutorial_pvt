d8c22e8a8f094198d1e3418469af1209947be4ca
highergear <ndrew.sebastian@gmail.com>
2023-06-05 16:42:24 +0700

    fix error

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM golang:1.20.1 as builder
+FROM golang:1.13 as builder
 WORKDIR /app
 COPY invoke.go ./
 RUN CGO_ENABLED=0 GOOS=linux go build -v -o server
 
-FROM ghcr.io/dbt-labs/dbt-bigquery:1.5.0
+FROM ghcr.io/dbt-labs/dbt-bigquery:1.5.latest
 USER root
 WORKDIR /dbt
 COPY --from=builder /app/server ./
--- invoke.go
+++ invoke.go
@@ -9,8 +9,9 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received a request to start dbt sequence")
+	log.Print("helloworld: received a request")
 	cmd := exec.Command("/bin/sh", "script.sh")
+	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
@@ -19,12 +20,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("starting server...")
+	log.Print("helloworld: starting server...")
 	http.HandleFunc("/", handler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	log.Printf("Listening on port %s\n", port)
+	log.Printf("helloworld: listening on %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
